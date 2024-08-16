989fc461ae3fe0a4ed513ad4f52408e470dfc718
vijay <vijaysparks21@gmail.com>
2024-08-16 14:28:08 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,200 @@
+
+// printing statement
+// console.log()
+
+// console.log("hello js") 
+// console.log("hello im working properly");  //comment out
+
+
+// three ways 
+
+// extension => coderunner 
+// terminal => node [filename]
+// browser 
+
+// variable types : 
+// var
+//  var a = 10   //redeclare
+//       a = 5   //reassign
+//  console.log(a);
+ 
+// let
+
+// let b = 10   //it can't redeclare
+//      b = 5   //reassign
+// console.log(b);
+
+// const
+
+// const c = 10   // it can't redeclare
+//       c = 5    // it can't reassign
+// console.log(c);
+
+// Datatypes : 
+// primitive datatype
+
+// string => "232434jdfkjhdkjhf2334343"
+// number =>   65654 & 56.65
+// boolean => true/false
+// undefined 
+// null
+
+// non primitive datatype 
+
+// array  [ ]  
+// collection of values and data within the square bracket is called array.
+
+// var arr = [1,2,3,4,5,6,7,8,9,10]
+// console.log(arr);
+
+// //object  { }
+// //collection of key and values pair
+
+// var obj = {
+//     //key :  value
+//     name : "gopal",
+//     age : 45,
+//     dep : "IT"
+// }
+
+// console.log(obj);
+
+
+// operators : 
+// Arithmetic  operator : 
+// + add  , - sub , * multiply , / division , % modulus , **exponent, ++ increment , -- decrement 
+
+// add: 
+
+var num = 10
+var num1 = 10
+var add = num + num1
+console.log(add);
+
+
+
+// //sub  
+
+// var num2 = 10
+// var num3 = 5
+// console.log(num2 - num3);
+
+// //mul 
+
+// var num4 = 5
+// var num5 = 5
+// console.log(num4*num5);
+
+// //div 
+
+// var num6 = 10
+// var num7 = 5
+// console.log(num6 / num7);
+
+// // modulus %
+
+// var num8 = 10
+// var num9 = 5
+// console.log(num8%num9);
+
+
+// //**  exponent 
+
+// var num10 = 2
+// var num11 = 5 
+// console.log(num10**num11); //2**5 => 2x2x2x2x2
+
+// //++increment  and  -- decrement 
+
+
+// var a = 10
+// a++   //10 + 1
+// console.log(a);
+
+// //decrement
+
+// var b = 15
+// b-- //15-1
+// console.log(b);
+// //assignment  operator :
+
+// var assn  = 10
+// assn += 5   // 10 += 5
+// console.log(assn);
+
+// //comparsion : 
+// //< lessthan , >greater than , <= lessthanorequals , >= greaterthanorequals 
+
+// //less than & greater than
+// var com1 = 10
+// var com2 = 15
+// console.log(com1>com2);
+
+// //lessthanorequals 
+
+// var le = 10
+// var le2 = 10
+// console.log(le>=le2);
+
+// //strict operators [interview question]
+
+// //double equals  => == => only check the values it will not check datatype
+
+//  var de = 5
+//  var de2 = "5"
+//  console.log(de==de2);
+ 
+// // 5 == "5"
+
+// //triple equals => === it check the value and also the datatypes .
+
+// let te = 5         //testcase => pass
+// let te2 = "5"      //testcase2 => fail
+// console.log(te===te2);  
+
+
+// //logical operator : 
+
+// // && => and
+// // || => or 
+// // ! => not
+
+// let not1 = 10
+// let not2 = 10
+// console.log(not1 != not2);
+
+// //Ternary operator  : 
+// //syntax : 
+// //(condition) ? value_true : value_false
+// let age = 1000
+// let limit = (age >= 18 && age<=70) ? "He is eligible to vote" : "He is not eligible to vote"
+// console.log(limit);
+
+// Function :   
+
+// function statement or declaration 
+// function expression
+// immmediate invoke function 
+// first class function 
+// arrow function 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
